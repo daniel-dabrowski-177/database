4f34edcbe048dc716f2dbe4f2bccbe37bf963234
Caster <daniel.dabrowski.177@gmail.com>
2023-09-04 12:58:47 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const arr = [
+  {
+    name: "LLC",
+    code: "1",
+  },
+  {
+    name: "Corporation",
+    code: "2",
+  },
+  {
+    name: "Sole Proprietor",
+    code: "3",
+  },
+];
+
+const express = require("express");
+const app = express();
+
+app.listen(precess.env.PORT, () => {
+  console.log("app is running");
+});
+
+app.get("/", (req, res) => {
+  res.send(arr);
+});
